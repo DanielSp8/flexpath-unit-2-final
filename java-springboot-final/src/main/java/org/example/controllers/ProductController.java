689de2a859e31fe058defaf350b5644dcc31d70b
Daniel Speight <danielspeight4@gmail.com>
2026-06-18 20:29:07 -0500

    GET all products test passes.

---
+++ java-springboot-final/src/main/java/org/example/controllers/ProductController.java
@@ -0,0 +1,26 @@
+package org.example.controllers;
+
+import org.example.daos.ProductDao;
+import org.example.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/products")
+
+public class ProductController {
+
+    @Autowired
+    private ProductDao productDao;
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productDao.getProducts();
+    }
+}
--- java-springboot-final/src/main/java/org/example/daos/ProductDao.java
+++ java-springboot-final/src/main/java/org/example/daos/ProductDao.java
@@ -1,4 +1,34 @@
 package org.example.daos;
 
+
+import org.example.models.Product;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Component
 public class ProductDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public List<Product> getProducts() {
+        return jdbcTemplate.query("SELECT * FROM products;", this::mapToProducts);
+    }
+
+    private Product mapToProducts(ResultSet resultSet, int rowNumber) throws SQLException {
+        int id = resultSet.getInt("id");
+        return new Product(
+                id,
+                resultSet.getString("name"),
+                resultSet.getBigDecimal("price")
+        );
+    }
+
 }
